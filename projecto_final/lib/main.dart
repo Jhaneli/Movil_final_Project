2057a4323f90be769abfac8e548c09a6e010d1bb
Jhaneli <moneko-chan@outlook.es>
2023-08-14 22:46:28 -0400

    2

--- projecto_final/lib/main.dart
+++ projecto_final/lib/main.dart
@@ -18,16 +18,50 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         drawer: Drawer(
-          backgroundColor: const Color.fromARGB(255, 229, 52, 226),
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.purple, // O cualquier otro color sólido
-            child: const Text(
+          backgroundColor: const Color.fromARGB(
+              255, 229, 52, 226), // O cualquier otro color sólido
+          child: Column(children: [
+            const Text(
               "Home",
-              style:
-                  TextStyle(color: Color.fromARGB(255, 1, 0, 0), fontSize: 20),
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 1, 0, 0),
+                  fontSize: 20),
             ),
-          ),
+            Container(
+              margin: const EdgeInsets.only(top: 30),
+              padding: const EdgeInsets.all(20),
+              width: double.maxFinite,
+              color: const Color.fromARGB(96, 123, 82, 111),
+              child: const Text("Home"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              width: double.maxFinite,
+              color: const Color.fromARGB(96, 123, 82, 111),
+              child: const Text("Maps"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              width: double.maxFinite,
+              color: const Color.fromARGB(96, 123, 82, 111),
+              child: const Text("Turisctics saves"),
+            ),
+            Expanded(child: Container()),
+            Container(
+              margin: const EdgeInsets.only(top: 2),
+              padding: const EdgeInsets.all(20),
+              width: double.maxFinite,
+              color: const Color.fromARGB(186, 0, 0, 0),
+              alignment: Alignment.center,
+              child: const Text(
+                "Sign out",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 253, 253, 253),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ]),
         ),
       ),
     );
